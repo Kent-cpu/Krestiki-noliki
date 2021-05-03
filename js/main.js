a5b0e02e92cc3e83f5f21bb32cb568b94bced5fe
Руслан Мирзаев <ruslanmirzaev16621@gmail.com>
2021-05-03 22:35:03 +0700

    Промежуточная версия

--- js/main.js
+++ js/main.js
@@ -1,35 +1,86 @@
 
 const field = JSON.parse(localStorage.getItem("field"));
-let circle =  `<svg class = "circle">
-<circle r = "20" cx = "30" cy = "30" stroke = "blue" stroke-width = "8" fill = "none" stroke-linecap = "round"/>
-</svg>`
-//document.querySelector(".restartGame").addEventListener("click", () => {
-//   
-//});
-
-
-function createField(){
-    const widthField = Number(field.Size) * 60 + 30;
-    document.querySelector(".gameField").style.width = widthField.toString() + "px";
-}
+const circle = `<svg class="circle">
+                    <circle r="20" cx="30" cy="30" stroke="blue" stroke-width="7" fill="none" stroke-linecap="round"></circle>
+                </svg>`;
+const cross = `<svg class="cross">
+<line class="first" x1="10" y1="10" x2="50" y2="50" stroke="red" stroke-width="7" stroke-linecap="round"></line>
+<line class="second" x1="50" y1="10" x2="10" y2="50" stroke="red" stroke-width="7" stroke-linecap="round"></line>
+</svg>`;
+let endGame = false;
+let current = "X";
+let arrField = [];
 
 function createCell(){
-    const gameField = document.querySelector(".gameField");
-    for(let numberCells = 0; numberCells < field.Size * field.Size; ++numberCells){
-        gameField.prepend(document.createElement("p"));
+    let gameField = document.querySelector(".gameField");
+    for(let line = 0; line < Number(field.Size); ++line){
+        let tr = gameField.insertRow();
+        let buf = [];
+        for(let numberCells = 0; numberCells < Number(field.Size); ++numberCells){
+            let td = tr.insertCell();
+            buf.push(td);
+        }
+        arrField.push(buf);
+    }
+}
+
+function checkEndGAme(row, col, symbol){
+    for(let i = 0, horizontal = 0, vertical = 0; i < Number(field.Size); ++i){
+        horizontal = arrField[row][i].innerHTML == symbol ?  ++horizontal : 0;
+        vertical = arrField[i][col].innerHTML == symbol ? ++vertical : 0;
+        if (horizontal == 3 || vertical == 3){
+            endGame = true; 
+            let winPlayer = symbol == cross ? "X" : "O";
+            document.querySelector(".winPlayer").textContent = "Выиграл: " + winPlayer;
+            return;
+        }
     }
 }
 
 
-createField();
+function determinationCourse(){
+    if(!endGame){
+        if(current == field.FirstGamer){
+            playerMove();   
+        }else{
+            moveBot();
+        }
+    }
+}
+
+function moveBot(){
+    let gameSymbol = field.FirstGamer == "X" ? circle : cross;
+    while(!endGame){
+        let row = Math.floor(Math.random() * Number(field.Size));
+        let column =  Math.floor(Math.random() * Number(field.Size));
+        if(arrField[row][column].innerHTML == ""){
+            arrField[row][column].innerHTML += gameSymbol;
+            current = field.FirstGamer == "X"  ? "X" : "O";
+            checkEndGAme(row, column , gameSymbol);
+            return;
+        }   
+    }
+}
+
+
+
 createCell();
+setInterval(determinationCourse, 50);
 
-
-document.querySelectorAll(".gameField p").forEach( (el) => {
-    el.addEventListener("click", e => {
-        //e.target.innerHTML += `<svg class = "circle">
-        //            <circle r = "20" cx = "30" cy = "30" stroke = "blue" stroke-width = "8" fill = "none" stroke-linecap = "round"/>
-        //        </svg>`;
-        e.target.innerHTML += circle;
-    })
-})
+function playerMove(){
+    if(!endGame){
+        arrField
+    }
+    for(let i = 0; i < Number(field.Size) && !endGame; ++i){
+        for(let j = 0; j < Number(field.Size); ++j){
+            arrField[i][j].addEventListener("click", e =>{
+                if(e.target.innerHTML == "" && e.target.tagName == "TD"){
+                    let gameSymbol = field.FirstGamer == "X" ? cross : circle;
+                    e.target.innerHTML += gameSymbol;
+                    checkEndGAme(i, j, gameSymbol);
+                    current = field.FirstGamer == "O" ? "X" : "O";
+                }
+            })
+        }
+    }
+}
